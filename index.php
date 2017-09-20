b94de17582d7d1f19e8494d2231e93546942e62c
Julien <papao1@hotmail.fr>
2017-09-20 10:52:18 +0200

    test lien layout Hello world

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+if(isset($_GET['p'])){
+    $p = $_GET['p'];
+}else{
+    $p = 'home';
+}
+ob_start();
+if($p === 'home'){
+    include('pages/home.php');
+}
+$content = ob_get_clean();
+include('assets/template/default.php');
+?>
