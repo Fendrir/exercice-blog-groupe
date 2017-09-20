06c23a4e33e26815f90f04545251f82c1a5754bb
Julien <papao1@hotmail.fr>
2017-09-20 15:28:39 +0200

    ajout d'une grosse partie de la nav-bar

--- assets/template/nav_bar.php
+++ assets/template/nav_bar.php
@@ -1,1 +1,88 @@
-<h1>ceci est ma navbar youhou !</h1>
+
+                                  <!-- Navbar sit web -->
+
+<nav class="navbar navbar-inverse bg-faded">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Mon super site</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="?p=home">Home</a></li>
+      <li><a href="?p=admin">Administrator dashboard</a></li>
+      <li><a href="?p=new_article">New article</a></li>
+    </ul>
+
+                <!-- partie recherche avec le menu déroulant -->
+
+    <ul class="nav navbar-nav navbar-right">
+    
+    <form class="navbar-form" method="GET" action="pages/recherche.php">
+
+    <div class="dropdown form-group">
+      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+      Filter
+        <span class="caret"></span>
+      </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+
+                    <!-- Filtre recherche 1 -->
+
+          <li class="dropdown-header">filtre à cocher 1</li>
+          
+          <div class="input-group">
+            <span class="input-group-addon">
+              <input type="checkbox" aria-label="filtre_1">
+            </span>
+            <p class="form-control">filtre 1</p>
+          </div>
+          <li role="separator" class="divider"></li>
+
+                    <!-- Filtre recherche 2 -->
+
+          <li class="dropdown-header">filtre à cocher 2</li>
+          
+          <div class="input-group">
+            <span class="input-group-addon">
+              <input type="checkbox" aria-label="filtre_2">
+            </span>
+            <p class="form-control">filtre 2</p>
+          </div>
+          <li role="separator" class="divider"></li>
+
+                    <!-- Filtre recherche 3 -->
+
+          <li class="dropdown-header">filtre à cocher 3</li>
+          
+          <div class="input-group">
+            <span class="input-group-addon">
+              <input type="checkbox" aria-label="filtre_3">
+            </span>
+            <p class="form-control">filtre 3</p>
+          </div>
+          <li role="separator" class="divider"></li>
+
+                    <!-- Filtre recherche 4 -->
+
+          <li class="dropdown-header">filtre à cocher 4</li>
+          
+          <div class="input-group">
+            <span class="input-group-addon">
+              <input type="checkbox" aria-label="filtre_4">
+            </span>
+            <p class="form-control">filtre 4</p>
+          </div>
+          <li role="separator" class="divider"></li>
+
+        </ul>
+    </div> 
+
+                    <!-- Bouton de recherche -->
+    
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search" name="recherche_nav">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+    </ul>
+  </div>
+</nav>
--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,3 @@
 <h1>Hello World</h1>
+
+<p>j'affiche ici de base tout mes articles en format réduit</p>
