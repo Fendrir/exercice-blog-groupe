4a99f78a204f083fbe47e3f7c495dc9a05ab861b
Julien <papao1@hotmail.fr>
2017-09-20 11:17:25 +0200

    tout les liens sont opérationnels pour l'espace de travail

--- assets/template/default.php
+++ assets/template/default.php
@@ -4,11 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
     
+    <header>
+    
+        <?php
+
+            include("nav_bar.php");
+
+        ?>
+    
+    </header>
+
     <?= $content ?>
+
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js" ></script>
+    <script src="js/app.js"></script>
 
 </body>
 </html>
---
+++ assets/template/nav_bar.php
@@ -0,0 +1,1 @@
+<h1>ceci est ma navbar youhou !</h1>
