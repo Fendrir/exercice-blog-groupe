ac4e9992dc6098fe397b3dc9b6b8cac664c730ad
mounga <christopher.fourgeaud@gmail.com>
2017-09-22 15:34:33 +0200

    Correction de quelques erreurs dans le fichier ajout_article.php

--- pages/ajout_article.php
+++ pages/ajout_article.php
@@ -1,48 +1,40 @@
 
-<h1>
-<p class="text-center">Ajouter un Article:</p>
-</h1>
-
-<div class="container">
+<h1 class="text-center">Ajouter un Article:</h1>
 
 <h2>Titre de l'article</h2>
-   <div class="input-group">
-  <input type="text" class="form-control" placeholder="">
- </div>
-</div>
+    <div class="input-group">
+  <input type="text" class="form-control" name="title" placeholder="">
+  </div>
+
 
 <div class="row">
-    <div class="col-md-4">
-  <h2>Auteur</h2>
-<div class="form-group has-success has-feedback"> 
-  <div class="input-group">
-  <input type="text" class="form-control" placeholder="">
+  <div class="col-md-4">
+    <h2>Auteur</h2>
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="">
+      </div>
   </div>
-  </div>
-    </div>
-  
-    
-  
+  <!-- Div vide pour faire un espace -->
   <div class="col-md-5"></div>
-    <div class="col-md-3"> 
-  <h2>Genre</h2>
-<div class="form-group">
-    <label for="disabledSelect"></label>
-      <select id="disabledSelect" class="form-control">
-        <option></option>
+  <div class="col-md-3"> 
+    <h2>Genre</h2>
+    <div class="form-group">
+      <select id="" class="form-control">
+        <option>test</option>
       </select>
-        </div>
-     </div>
+    </div>
   </div>
+</div>
 
 <h2>Contenu</h2>
 <textarea class="form-control" rows="6s"></textarea>
 
-<p>
+<br>
+
 <div class="row">
-  <div class="col-md-4"></div>
-    <div class="col-md-">  <button type="button" class="btn btn-lg btn-success">Valider</button>
+  <div class="col-md-5 col-md-offset-5">
+    <button type="button" class="btn btn-lg btn-success">Valider</button>
+  </div>
 </div>
-    <div class="col-md-4"></div>
-</div> 
-<p>
+
+
